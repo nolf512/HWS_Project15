46ec8ff2cae78fb48dba6e15d95636c24a0b1683
nofprog <k.jin3217@gmail.com>
2021-04-23 18:24:38 +0900

    画像の表示, アニメーションのクロージャ処理

--- HWS_Project15/ViewController.swift
+++ HWS_Project15/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //画面の中央に画像を配置
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 596, y: 384)
+        view.addSubview(imageView)
+
+
+
     }
 
-
+    @IBAction func tapped(_ sender: UIButton) {
+        
+        //UIButtonを非表示にする
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                break
+                
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+       }
+        
+        currentAnimation += 1
+        
+        //7までいったら0にする
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+        print(currentAnimation)
+    }
+    
 }
 
